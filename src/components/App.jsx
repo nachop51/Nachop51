1b377d873518a44b9faf18c0e2c222fe658b1e8e
Nachop51 <nachoperalta0@gmail.com>
2022-10-31 19:37:57 -0300

    dummy components

---
+++ src/components/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import NavBar from "./NavBar";
+
+import React from "react";
+
+const App = () => {
+  return (
+    <>
+      <NavBar />
+      <h1 className="text-3xl font-light">Hello World</h1>
+    </>
+  );
+};
+
+export default App;
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const NavBar = () => {
+  return (
+    <ul>
+      <li>
+        <span className="font-bold">About me</span>;
+      </li>
+      <li>
+        <span className="font-bold">Projects</span>;
+      </li>
+      <li>
+        <span className="font-bold">Contact</span>;
+      </li>
+    </ul>
+  );
+};
+
+export default NavBar;
