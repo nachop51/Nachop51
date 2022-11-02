5f3ef1f3d64a754d23dc265f49ef473086af7c79
Nachop51 <nachoperalta0@gmail.com>
2022-11-02 20:51:38 -0300

    base things added

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,7 +7,13 @@ const App = () => {
   return (
     <>
       <NavBar />
-      <h1 className="text-3xl font-light">Hello World</h1>
+      <h1 className="text-3xl font-light">
+        Hi, my name is <b>Nacho</b>
+        <span>.</span>
+      </h1>
+      <p>
+        I'm a <strong>full-stack developer</strong> from <b>Uruguay</b>.
+      </p>
     </>
   );
 };
