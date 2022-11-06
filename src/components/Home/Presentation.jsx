1279995fa2f9bc202ec118412944d483df557321
Nachop51 <nachoperalta0@gmail.com>
2022-11-05 22:20:32 -0300

    fixing scrollbar

--- src/components/Home/Presentation.jsx
+++ src/components/Home/Presentation.jsx
@@ -3,12 +3,12 @@ import "./Presentation.css";
 const Presentation = () => {
   return (
     <section id="presentation">
-      <h1 className="text-3xl font-light">
+      <h1 className="heading-primary">
         Hi, my name is <b>Nacho</b>
         <span className="dot">.</span>
       </h1>
-      <p>
-        I'm a <strong>full-stack developer</strong> from <b>Uruguay</b>.
+      <p className="presentation__p">
+        I'm a passionate <strong>Software Engineer</strong> from <b>Uruguay</b>.
       </p>
     </section>
   );
