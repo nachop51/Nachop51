e30b6c9c1e66ebe8f57f47075016258add261610
Nachop51 <nachoperalta0@gmail.com>
2022-11-09 21:08:43 -0300

    first try navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,30 +4,34 @@ import { Link } from "react-router-dom";
 
 const NavBar = () => {
   return (
-    <nav>
-      <ul className="nav-list">
-        <li>
-          <Link to="/">
-            <span className="links">Home</span>
-          </Link>
-        </li>
-        <li>
-          <Link to="about_me">
-            <span className="links">About me</span>
-          </Link>
-        </li>
-        <li>
-          <Link to="work">
-            <span className="links">Work</span>
-          </Link>
-        </li>
-        <li>
-          <Link to="contact">
-            <span className="links">Contact</span>
-          </Link>
-        </li>
-      </ul>
-    </nav>
+    <header className="header">
+      <nav className="nav">
+        <ul className="nav-list">
+          <li>
+            <Link to="/">
+              <span className="links">Home</span>
+            </Link>
+          </li>
+          <li>
+            <Link to="about_me">
+              <span className="links">About me</span>
+            </Link>
+          </li>
+          <li className="menu"></li>
+          <li>
+            <Link to="work">
+              <span className="links">Work</span>
+            </Link>
+          </li>
+          <li>
+            <Link to="contact">
+              <span className="links">Contact</span>
+            </Link>
+          </li>
+        </ul>
+        <button className="menu-button">-</button>
+      </nav>
+    </header>
   );
 };
 
