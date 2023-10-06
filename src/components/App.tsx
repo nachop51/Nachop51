c8ceb49192a50e155b5a820afecd346c578f3907
Nachop51 <nachoperalta0@gmail.com>
2023-10-05 21:41:29 -0300

    refactor

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,14 +1,44 @@
-import Home from "./Home"
-import Media from "./Media"
-import NavBar from "./NavBar"
-import "./App.css"
+import { FiGithub, FiInstagram, FiTwitter, FiLinkedin } from 'react-icons/fi'
+import Home from './Home'
+import './Media.css'
+import NavBar from './NavBar'
+import './App.css'
+
+const mediaIcons = [
+  { name: 'Github', link: 'https://github.com/Nachop51/', icon: <FiGithub /> },
+  {
+    name: 'Instagram',
+    link: 'https://instagram.com/nachop51/',
+    icon: <FiInstagram />,
+  },
+  {
+    name: 'Twitter',
+    link: 'https://twitter.com/inach0f',
+    icon: <FiTwitter />,
+  },
+  {
+    name: 'Linkedin',
+    link: 'https://linkedin.com/in/ignacio-peralta-dev/',
+    icon: <FiLinkedin />,
+  },
+]
 
 function App() {
+  const renderedIcons = mediaIcons.map((icon) => (
+    <li key={icon.name}>
+      <a href={icon.link} target="_blank" rel="noopener noreferrer">
+        {icon.icon}
+      </a>
+    </li>
+  ))
+
   return (
     <>
       <NavBar />
       <Home />
-      <Media />
+      <div className="media">
+        <ul className="media__ul">{renderedIcons}</ul>
+      </div>
     </>
   )
 }
