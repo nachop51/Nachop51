e58e5cada7275e4f10b598e879775e1b9631dea0
Nachop51 <nachoperalta0@gmail.com>
2024-01-17 23:47:20 -0300

    Daisyui added, refactor in presentation and navbar components

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,39 +1,47 @@
 import './NavBar.css'
 import { Link } from 'react-router-dom'
 import { BsMailbox } from 'react-icons/bs'
-import { useState } from 'react'
+import { useRef, useState } from 'react'
 
 const NavBar = () => {
   const [isOpen, setIsOpen] = useState(false)
+  const ref = useRef<HTMLDialogElement | null>(null)
 
   const handleClick = () => {
     setIsOpen(true)
   }
 
+  if (isOpen && ref.current != null) {
+    ref.current.showModal()
+  } if (!isOpen && ref.current != null) {
+    ref.current.close()
+  }
+
   return (
     <>
-      {
-        isOpen &&
-          <dialog open className='m-auto z-50'>
-            <button onClick={() => { setIsOpen(false) }}>Close</button>
-            <div className='flex flex-col gap-2'>
-              <Link to='/'>Home</Link>
-              <Link to='/about'>About</Link>
-              <Link to='/projects'>Projects</Link>
-              <Link to='/contact'>Contact</Link>
-            </div>
-          </dialog>
-      }
+      <dialog ref={ref} className='m-auto bg-transparent'>
+        <nav>
+          <div className='flex flex-col gap-2'>
+            <Link to='/'>Home</Link>
+            <Link to='/about'>About</Link>
+            <Link to='/projects'>Projects</Link>
+            <Link to='/contact'>Contact</Link>
+          </div>
+        </nav>
+        <button className='btn btn-primary' onClick={() => { setIsOpen(false) }}>Close</button>
+      </dialog>
       <div className='navbar'>
         <div>
           <Link to='/' className='cursor-pointer'>
             Icon
           </Link>
-          |
+          <span className='mx-3'>
+            |
+          </span>
           <button onClick={handleClick}>Menu</button>
         </div>
         <div className='flex items-center gap-2'>
-          <BsMailbox />Reach me
+          <BsMailbox /> Reach me
         </div>
       </div>
     </>
--- src/pages/Home/Presentation.tsx
+++ src/pages/Home/Presentation.tsx
@@ -1,24 +1,34 @@
 import Nachop from '../../components/Nachop'
+import './Presentation.css'
 
 const Presentation = () => {
+  const scrollDown = 'SCROLL'.split('').map((letter, index) => (
+    <span key={`${letter} + ${index}`} style={{ animationDelay: `${index * 200}ms` }}>
+      {letter}
+    </span>
+  ))
+
   return (
     <section className='set-height' id='presentation'>
-      <div className='presentation-avatar'>
-        <div className='background-deco' />
-        <Nachop className='drawing' />
+      <div className='presentation-wrapper'>
+        <div className='presentation-avatar'>
+          <div className='background-deco' />
+          <Nachop className='drawing' />
+        </div>
+        <header className='presentation-header'>
+          <h1 className='heading-primary'>
+            Hi, my name is <b className='font-bold'>Nacho</b>
+            <span className='dot'>.</span>
+          </h1>
+          <p className='heading-paragraph'>
+            I'm a <strong>passionate Software Engineer</strong> from{' '}
+            <b>Uruguay</b>.
+          </p>
+        </header>
       </div>
-      <header className='presentation-header'>
-        <h1 className='heading-primary'>
-          Hi, my <br className='hidden lg:block' /> name is <b className='font-bold'>Nacho</b>
-          <span className='dot'>.</span>
-        </h1>
-        <p className='heading-paragraph'>
-          I'm a <strong>passionate Software Engineer</strong> from{' '}
-          <b>Uruguay</b>.
-        </p>
-      </header>
       <span className='scroll-down'>
-        👇🏼
+        {scrollDown}
+        <i className='bar' />
       </span>
     </section>
   )
