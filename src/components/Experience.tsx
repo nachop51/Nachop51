c2af44921d3c261c7ebdcb3f0bf4db692524b3c0
Nachop51 <nachoperalta0@gmail.com>
2024-03-04 23:57:34 -0300

    Tech stack update

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -1,19 +1,6 @@
-const Experience = () => {
-  const experience = [
-    {
-      date: 'Nov 2022 - Present',
-      role: 'Software Engineer',
-      company: 'Holberton',
-      description: 'I\'m a Junior Software Engineer who can combine technical expertise with clear communication. As a collaborative team player, I contribute to student progress tracking and integrate cutting-edge technologies. My focus is on efficiency through the use of software development and automation. With a strong technical foundation and effective communication skills, I\'m geared to make an impactful contribution to project success and team performance.'
-    },
-    {
-      date: 'Jun 2022 - Nov 2022',
-      role: 'Tutor',
-      company: 'Holberton',
-      description: 'At Holberton, I have honed my technical expertise while developing crucial soft skills. From delivering powerful technical speeches to adapting communications for diverse audiences, my abilities extend to effective teaching, motivating students and conducting insightful evaluations. These skills uniquely position me to support and encourage the growth of aspiring software developers.'
-    }
-  ]
+import { experience } from '@/constants'
 
+const Experience = () => {
   return (
     <ol className='relative border-s border-base-content'>
       {
--- src/constants.tsx
+++ src/constants.tsx
@@ -1,5 +1,5 @@
-import { type TechnologyItemType } from './types'
-import { AWSIcon, CSSIcon, DockerIcon, ExpressIcon, GitHubIcon, GitIcon, HTMLIcon, JavaScriptIcon, MySQLIcon, NextJSIcon, NginxIcon, NodeJSIcon, PostgresIcon, PythonIcon, ReactIcon, SvelteIcon, TailwindCSSIcon, TypeScriptIcon, VSCodeIcon, VercelIcon } from './components/Icons'
+import { type ProjectsType, type TechnologyItemType } from './types'
+import { AWSIcon, CSSIcon, DockerIcon, ExpressIcon, FigmaIcon, GitHubIcon, GitIcon, HTMLIcon, JavaScriptIcon, MongoIcon, MySQLIcon, NextJSIcon, NginxIcon, NodeJSIcon, PostgresIcon, PythonIcon, ReactIcon, SvelteIcon, TailwindCSSIcon, TypeScriptIcon, VSCodeIcon, VercelIcon } from './components/Icons'
 
 export const TECH_TAGS = {
   React: {
@@ -101,8 +101,88 @@ export const TECH_TAGS = {
     name: 'Python',
     icon: <PythonIcon />,
     color: '#3776AB'
+  },
+  MongoDB: {
+    name: 'MongoDB',
+    icon: <MongoIcon />,
+    color: '#47A248'
+  },
+  Figma: {
+    name: 'Figma',
+    icon: <FigmaIcon />,
+    color: '#F24E1E'
   }
 } as const
+
+export const experience = [
+  {
+    date: 'Nov 2022 - Present',
+    role: 'Software Engineer',
+    company: 'Holberton',
+    description: 'I\'m a Junior Software Engineer who can combine technical expertise with clear communication. As a collaborative team player, I contribute to student progress tracking and integrate cutting-edge technologies. My focus is on efficiency through the use of software development and automation. With a strong technical foundation and effective communication skills, I\'m geared to make an impactful contribution to project success and team performance.'
+  },
+  {
+    date: 'Jun 2022 - Nov 2022',
+    role: 'Tutor',
+    company: 'Holberton',
+    description: 'At Holberton, I have honed my technical expertise while developing crucial soft skills. From delivering powerful technical speeches to adapting communications for diverse audiences, my abilities extend to effective teaching, motivating students and conducting insightful evaluations. These skills uniquely position me to support and encourage the growth of aspiring software developers.'
+  }
+] as const
+
+export const projects: ProjectsType[] = [
+  {
+    title: 'Nachortener',
+    description: 'Simplify your long URLs into short shareable links! Easily customize and track your links for effective online sharing, change the visibility of your link. Say more with less. Try nachortener now!',
+    images: '/images/short_nachop.webp',
+    repos: ['https://github.com/Nachop51/Nachop51'],
+    links: ['https://short.nachop.xyz/'],
+    tags: [
+      TECH_TAGS.Svelte,
+      TECH_TAGS.TypeScript,
+      TECH_TAGS.TailwindCSS
+    ]
+  },
+  {
+    title: 'Nachop Theme',
+    description: 'A dark theme for Visual Studio Code, inspired by the best themes out there. It is designed to be easy on the eyes, while providing all the important information you need to get your work done. It is also designed to be easy to read, with a clean and simple design.',
+    images: '/images/nachop-theme.webp',
+    repos: ['https://github.com/Nachop51/nachop-theme'],
+    links: ['https://marketplace.visualstudio.com/items?itemName=Nachop51.nachop-theme'],
+    tags: [
+      TECH_TAGS.VSCode,
+      TECH_TAGS.JavaScript
+    ]
+  },
+  {
+    title: 'Paid websites',
+    description: 'Specific websites for paid projects. This projects are for Holberton School Uruguay. Strategicly designed to attract potential clients and show the potential of the students.',
+    images: ['/images/paid_1.webp', '/images/paid_2.webp'],
+    repos: ['https://github.com/Nachop51/hbtn-webs/tree/main/it_builders-web', 'https://github.com/Nachop51/hbtn-webs/tree/main/c20-linktree'],
+    links: ['https://holbertonschool.uy/a-holbie-in-your-team', 'https://holbertonschool.uy/c20-demoday'],
+    tags: [
+      TECH_TAGS.HTML,
+      TECH_TAGS.TailwindCSS,
+      TECH_TAGS.NodeJS
+    ]
+  },
+  {
+    title: 'Loopit',
+    description: 'Loopit is a social media platform for programmers to share their code with the world. Create a profile, upload your code, and share it with your friends and followers.',
+    images: '/images/loopit.webp',
+    repos: ['https://github.com/Nachop51/loopit'],
+    links: [],
+    tags: [
+      TECH_TAGS.React,
+      TECH_TAGS.JavaScript,
+      TECH_TAGS.NodeJS,
+      TECH_TAGS.Express,
+      TECH_TAGS.MySQL,
+      TECH_TAGS.Docker,
+      TECH_TAGS.Nginx,
+      TECH_TAGS.AWS
+    ]
+  }
+] as const
 
 export const technologiesSections: TechnologyItemType[] = [
   {
@@ -121,7 +201,10 @@ export const technologiesSections: TechnologyItemType[] = [
       {
         ...TECH_TAGS.TailwindCSS,
         glow: true
-      }
+      },
+      TECH_TAGS.HTML,
+      TECH_TAGS.CSS,
+      TECH_TAGS.JavaScript
     ]
   },
   {
@@ -130,9 +213,10 @@ export const technologiesSections: TechnologyItemType[] = [
       TECH_TAGS.NodeJS,
       TECH_TAGS.Express,
       TECH_TAGS.Postgres,
+      TECH_TAGS.Python,
+      TECH_TAGS.MongoDB,
       TECH_TAGS.AWS,
-      TECH_TAGS.Nginx,
-      TECH_TAGS.Python
+      TECH_TAGS.Nginx
     ]
   },
   {
@@ -143,7 +227,8 @@ export const technologiesSections: TechnologyItemType[] = [
       TECH_TAGS.Git,
       TECH_TAGS.GitHub,
       TECH_TAGS.Vercel,
-      TECH_TAGS.VSCode
+      TECH_TAGS.VSCode,
+      TECH_TAGS.Figma
     ]
   }
 ]
